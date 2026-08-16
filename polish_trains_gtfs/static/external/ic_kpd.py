c353ea64e39bcc19307376653c646d16799e9470
Marcin Kasznia <kasmar00@gmail.com>
2026-08-16 22:18:15 +0200

    Review fixes - refactor to extract the main algorithm

--- polish_trains_gtfs/static/external/ic_kpd.py
+++ polish_trains_gtfs/static/external/ic_kpd.py
@@ -13,7 +13,7 @@ from itertools import groupby
 from operator import itemgetter
 
 from impuls import LocalResource, Task, TaskRuntime
-from impuls.model import StopTime, Trip, CalendarException, Stop
+from impuls.model import StopTime, TimePoint, Trip, CalendarException, Stop
 from impuls.errors import InputNotModified
 from impuls.resource import ConcreteResource, Resource, ZippedResource
 from impuls.tools.types import StrPath
@@ -79,6 +79,8 @@ class CleanWaypoints(Task):
 class LoadICKPD(LoadExternal):
     def __init__(self):
         super().__init__()
+        self.important_waypoints = set[str]()
+        self.calendar_lookup = CalendarLookup(list)
 
     @staticmethod
     def get_required_resources() -> dict[str, Resource]:
@@ -100,18 +102,32 @@ class LoadICKPD(LoadExternal):
         else:
             return {}
 
+    def clear(self):
+        self.important_waypoints.clear()
+        self.calendar_lookup.clear()
+
+
     def execute(self, r: TaskRuntime) -> None:
-        if "ic_kpd_rozklad.csv" not in r.resources:
-            self.logger.warning(
-                "No IC KPD Rozklad resource available. Skipping execution."
-            )
+        self.clear()
+        self.load_lookup_tables(r)
+        kpd = self.load_kpd(r)
+
+        if not kpd:
             return
 
-        important_waypoints = {cast(str, i) for i in r.resources["waypoints.yaml"].yaml()["waypoints"]}
+        for plk_trip in self.get_trips_to_process(r):
+            if kpd_times := self.find_kpd_match(kpd, plk_trip, r):
+                plk_times = self.get_plk_stop_times(plk_trip, r)
+                combined_times = self.enhance_items(plk_times, kpd_times, r)
+                if combined_times:
+                    self.apply_combined_times(plk_trip, combined_times, r)
+
+    def load_lookup_tables(self, r: TaskRuntime):
+        self.important_waypoints = {cast(str, i) for i in r.resources["waypoints.yaml"].yaml()["waypoints"]}
 
         with r.db.transaction():
             all_stops = {stop.id for stop in r.db.retrieve_all(Stop).all()}
-            for stop_id in important_waypoints:
+            for stop_id in self.important_waypoints:
                 if stop_id not in all_stops:
                     r.db.create(
                         Stop(
@@ -123,122 +139,124 @@ class LoadICKPD(LoadExternal):
                     )
                     all_stops.add(stop_id)
 
-        rows = train_rows(r.resources["ic_kpd_rozklad.csv"].stored_at, important_waypoints)
-
-        kpd_lookup = build_kpd_lookup(rows)
-
-        trips = r.db.typed_out_execute("SELECT * FROM trips WHERE trip_id LIKE 'PLK_IC_%'", Trip).all()
         calendar_dates = r.db.retrieve_all(CalendarException).all()
-        calendar_lookup = build_calendar_lookup(calendar_dates)
+        for entry in calendar_dates:
+            self.calendar_lookup[entry.calendar_id].append(str(entry.date))
 
-        for trip in trips:
-            plk_number = trip.get_extra_field("plk_train_number")
-            if not plk_number:
-                self.logger.warning("Trip %s has no plk_train_number", trip.id)
-                continue
-
-            main_number, extra_number = get_plk_train_numbers(plk_number)
-
-            if main_number not in kpd_lookup:
-                if extra_number in kpd_lookup:
-                    main_number = extra_number
-                else:
-                    self.logger.warning(
-                        "Train %s not found in KPD Rozklad data.", main_number
-                    )
-                    continue
-
-            calendar_dates_for_trip = calendar_lookup.get(trip.calendar_id, [])
-            if not calendar_dates_for_trip:
-                self.logger.warning(
-                    "Trip %s has no calendar dates. Skipping.", trip.id
-                )
-                continue
-
-            date = next((d for d in calendar_dates_for_trip if d in kpd_lookup[main_number]), None)
-            if not date:
-                self.logger.warning(
-                    f"Train {trip.id} / {main_number} has no stops in KPD for any of its active dates. Skipping."
-                )
-                continue
-            stops_from_kpd = kpd_lookup[main_number][date]
-            stops_from_plk = r.db.typed_out_execute(
-                "SELECT * FROM stop_times WHERE trip_id = ? ORDER BY stop_sequence",
-                StopTime,
-                (trip.id,),
-            ).all()
-
-            combined, diverging = merge_stop_sequences(
-                stops_from_plk, stops_from_kpd
+    def load_kpd(self, r: TaskRuntime) -> KPDLookup | None:
+        if "ic_kpd_rozklad.csv" not in r.resources:
+            self.logger.warning(
+                "No IC KPD Rozklad resource available. Skipping execution."
             )
+            return
 
-            if not diverging:
-                continue
+        rows = train_rows(r.resources["ic_kpd_rozklad.csv"].stored_at, self.important_waypoints)
 
-            filtered = ensure_start_and_end_at_pax_station(combined)
-            try:
-                r.db.raw_execute(
-                    """
-                                DELETE FROM stop_times
-                                WHERE trip_id = ?
-                                """,
-                    (trip.id,),
+        return build_kpd_lookup(rows)
+
+
+    def get_trips_to_process(self, r:TaskRuntime) -> list[Trip]:
+        return r.db.typed_out_execute("SELECT * FROM trips WHERE trip_id LIKE 'PLK_IC_%'", Trip).all()
+
+    def find_kpd_match(self, kpd: KPDLookup, trip: Trip, r: TaskRuntime) -> list[KPDStop] | None:
+
+        plk_number = trip.get_extra_field("plk_train_number")
+        if not plk_number:
+            self.logger.warning("Trip %s has no plk_train_number", trip.id)
+            return
+
+        main_number, extra_number = get_plk_train_numbers(plk_number)
+
+        if main_number not in kpd:
+            if extra_number in kpd:
+                main_number = extra_number
+            else:
+                self.logger.warning(
+                    "Train %s not found in KPD Rozklad data.", main_number
                 )
-                r.db.raw_execute_many(
-                    """
-                INSERT INTO stop_times (stop_id, trip_id, stop_sequence,
-                                arrival_time, departure_time, pickup_type, drop_off_type, stop_headsign, shape_dist_traveled, platform, extra_fields_json)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            """,
-                    [
-                        (
-                            stop_id,
-                            trip.id,
-                            i + 1,
-                            int(plk.arrival_time.total_seconds()) if plk else 0,
-                            int(plk.departure_time.total_seconds()) if plk else 0,
-                            plk.pickup_type.value if plk else 1,
-                            plk.drop_off_type.value if plk else 1,
-                            plk.stop_headsign if plk else "",
-                            plk.shape_dist_traveled if plk else None,
-                            merge_platform(plk, kpd),
-                            merge_extra_fields(plk, kpd),
-                        )
-                        for i, (stop_id, plk, kpd) in enumerate(filtered)
-                    ],
-                )
-            except Exception:
-                self.logger.error(
-                    f"Error occurred while updating stop_times for trip {trip.id} / {main_number}",
-                    exc_info=True,
-                )
-                self.logger.debug(
-                    f"Stops for trip {trip.id} / {main_number}: {filtered}"
-                )
-                raise
+                return
+
+        calendar_dates_for_trip = self.calendar_lookup.get(trip.calendar_id, [])
+        if not calendar_dates_for_trip:
+            self.logger.warning(
+                "Trip %s has no calendar dates. Skipping.", trip.id
+            )
+            return
+
+        date = next((d for d in calendar_dates_for_trip if d in kpd[main_number]), None)
+        if not date:
+            self.logger.warning(
+                f"Train {trip.id} / {main_number} has no stops in KPD for any of its active dates. Skipping."
+            )
+            return
+        stops_from_kpd = kpd[main_number][date]
+        return stops_from_kpd
+
+    def get_plk_stop_times(self, trip: Trip, r: TaskRuntime) -> list[StopTime]:
+        stops_from_plk = r.db.typed_out_execute(
+            "SELECT * FROM stop_times WHERE trip_id = ? ORDER BY stop_sequence",
+            StopTime,
+            (trip.id,),
+        ).all()
+        return stops_from_plk
+
+    def enhance_items(self, plk_stop_times: list[StopTime], kpd_stops: list[KPDStop], r: TaskRuntime) -> list[StopTime] | None:
+        """
+        Enhances PLK stop times with KPD stop data and inserts KPD stops when missing from PLK.
+        """
+        keys_plk = [v.stop_id for v in plk_stop_times]
+        keys_kpd = [v.stop_id for v in kpd_stops]
+    
+        combined = list[StopTime]()
+        updated_stops = set[str]()
+        trip_id = plk_stop_times[0].trip_id
+    
+        matcher = difflib.SequenceMatcher(None, keys_plk, keys_kpd)
+    
+        for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+            if tag == "equal":
+                for plk, kpd in zip(plk_stop_times[i1:i2], kpd_stops[j1:j2]):
+                    stop_time, updated = merge_stop_time_with_kpd(plk, kpd)
+                    combined.append(stop_time)
+                    if updated:
+                        updated_stops.add(plk.stop_id)
+            else:
+                if tag in ("replace", "delete"):
+                    for plk in plk_stop_times[i1:i2]:
+                        combined.append(plk)
+                if tag in ("replace", "insert"):
+                    for kpd in kpd_stops[j1:j2]:
+                        combined.append(kpd_to_stop_time(trip_id, kpd))
+                        updated_stops.add(kpd.stop_id)
+    
+        if not updated_stops:
+            return None
+    
+        for i in range(len(combined)):
+            combined[i].stop_sequence = i
+    
+        return ensure_start_and_end_not_at_waypoint(combined)
 
 
-def merge_platform(plk: StopTime | None, kpd: KPDStop | None) -> str:
-    if plk and plk.platform:
-        return plk.platform
-
-    if kpd:
-        return normalize_platform(kpd.departure_platform)
-
-    return ""
-
-
-def merge_extra_fields(plk: StopTime | None, kpd: KPDStop | None) -> str | None:
-    """
-    Add track number when it's missing in plk data
-    """
-    if not plk:
-        return None
-
-    if not plk.platform and kpd:
-        plk.set_extra_field("track", kpd.departure_track)
-
-    return plk.extra_fields_json
+    def apply_combined_times(self, trip: Trip, combined: list[StopTime], r: TaskRuntime):
+        try:
+            r.db.raw_execute(
+                """
+                            DELETE FROM stop_times
+                            WHERE trip_id = ?
+                            """,
+                (trip.id,),
+            )
+            r.db.create_many(StopTime, combined)
+        except Exception:
+            self.logger.error(
+                f"Error occurred while updating stop_times for trip {trip.id} {trip.get_extra_field('plk_train_number')}",
+                exc_info=True,
+            )
+            self.logger.debug(
+                f"Stops for trip {trip.id}: {combined}"
+            )
+            raise
 
 
 def normalize_platform(x: str) -> str:
@@ -269,35 +287,27 @@ def get_plk_train_numbers(plk_number: str) -> tuple[str, str]:
     return main_number, extra_number
 
 
-def merge_stop_sequences(
-    stops_from_plk: list[StopTime], stops_from_kpd: list[KPDStop]
-) -> tuple[list[tuple[str, StopTime | None, KPDStop | None]], set[str]]:
+def merge_stop_time_with_kpd(plk: StopTime, kpd: KPDStop) -> tuple[StopTime, bool]:
+    updated = False
+    if not plk.platform:
+        plk.platform = normalize_platform(kpd.departure_platform)
+        plk.set_extra_field("track", kpd.departure_track)
+        updated = True
+    return plk, updated
+
+def kpd_to_stop_time(trip_id: str, kpd: KPDStop):
     """
-    Merges PLK and KPD stop sequences and identifies diverging stop IDs.
+    Creates an "empty" StopTime based on KPDStop. Such StopTime mustn't be exported in GTFS and must be removed via CleanWaypoints
     """
-    keys_plk = [v.stop_id for v in stops_from_plk]
-    keys_kpd = [v.stop_id for v in stops_from_kpd]
-
-    combined: list[tuple[str, StopTime | None, KPDStop | None]] = []
-    diverging: set[str] = set()
-
-    matcher = difflib.SequenceMatcher(None, keys_plk, keys_kpd)
-
-    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
-        if tag == "equal":
-            for plk, kpd in zip(stops_from_plk[i1:i2], stops_from_kpd[j1:j2]):
-                combined.append((plk.stop_id, plk, kpd))
-        else:
-            if tag in ("replace", "delete"):
-                for k in stops_from_plk[i1:i2]:
-                    combined.append((k.stop_id, k, None))
-                    diverging.add(k.stop_id)
-            if tag in ("replace", "insert"):
-                for k in stops_from_kpd[j1:j2]:
-                    combined.append((k.stop_id, None, k))
-                    diverging.add(k.stop_id)
-
-    return combined, diverging
+    return StopTime(
+        stop_id=kpd.stop_id,
+        trip_id=trip_id,
+        stop_sequence=0,
+        arrival_time=TimePoint(),
+        departure_time=TimePoint(),
+        pickup_type=StopTime.PassengerExchange.NONE,
+        drop_off_type=StopTime.PassengerExchange.NONE,
+    )
 
 
 def build_kpd_lookup(rows: Iterator[tuple[TrainKey, Iterator[CSVRow]]]) -> KPDLookup:
@@ -318,27 +328,18 @@ def build_kpd_lookup(rows: Iterator[tuple[TrainKey, Iterator[CSVRow]]]) -> KPDLo
             stops.append(KPDStop(stop_id, departure_platform, departure_track))
         parsed[clean_number][key.date] = stops
     return parsed
+    
 
 
-def build_calendar_lookup(calendar_dates: list[CalendarException]) -> CalendarLookup:
-    """
-    Builds a lookup mapping calendar IDs to a list of dates (as strings).
-    """
-    lookup = CalendarLookup(list)
-    for entry in calendar_dates:
-        lookup[entry.calendar_id].append(str(entry.date))
-    return lookup
-
-
-def ensure_start_and_end_at_pax_station(
-    stops: list[tuple[str, StopTime | None, KPDStop | None]],
-) -> list[tuple[str, StopTime | None, KPDStop | None]]:
+def ensure_start_and_end_not_at_waypoint(
+    stops_times: list[StopTime]
+) -> list[StopTime]:
     def is_waypoint(index: int) -> bool:
-        kpd_stop = stops[index][2]
-        return kpd_stop is not None and kpd_stop.departure_platform == "WAYPOINT"
+        stop_time = stops_times[index]
+        return stop_time.pickup_type == StopTime.PassengerExchange.NONE and stop_time.drop_off_type == StopTime.PassengerExchange.NONE
 
     start = 0
-    end = len(stops) - 1
+    end = len(stops_times) - 1
 
     while start <= end and is_waypoint(start):
         start += 1
@@ -346,7 +347,7 @@ def ensure_start_and_end_at_pax_station(
     while end >= start and is_waypoint(end):
         end -= 1
 
-    return stops[start : end + 1]
+    return stops_times[start : end + 1]
 
 
 def train_rows(filename: StrPath, important_waypoints: set[str]) -> Iterator[tuple[TrainKey, Iterator[CSVRow]]]:
@@ -444,5 +445,4 @@ ROMAN_TO_ARABIC = {
     "X": "10",
     "XI": "11",
     "XII": "12",
-    "WAYPOINT": "WAYPOINT",
 }
